838bb7ef8d945abbf444fa0bc7b107595f4bdd4e
Ryan Large <ryanhudsonlarge13@gmail.com>
2025-08-28 14:07:20 -0400

    HREF Update

--- load.js
+++ load.js
@@ -98,7 +98,7 @@ const storeUserInApp = (name, email) => {
 
     localStorage.setItem("user", localStorageUser);
 
-    window.location.href = "/";
+    window.location.href = "Tomato/index.html";
   } catch (err) {
     console.log(`Error loading localStorage. ${err}`);
   }
